772655bf9aa735c09efd43e2f35b11e9d313bded
Yogesh Patil <officialyogeshp@gmail.com>
2022-02-09 23:02:09 +0530

    Application structure setup

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  res.status(404).json({
+    message: "No such route exists",
+  });
+});
+
+module.exports = app;
